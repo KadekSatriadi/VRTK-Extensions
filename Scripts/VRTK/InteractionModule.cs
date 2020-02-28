9a8e83cd731dc962601c975d5ab337205e966584
Kadek Satriadi <ananta.satriadi@gmail.com>
2020-02-28 22:50:15 +1100

    InteractionModule and InteractionPolicy

---
+++ Scripts/VRTK/InteractionModule.cs
@@ -0,0 +1,24 @@
+﻿using VRTK;
+using UnityEngine;
+
+public abstract class InteractionModule : MonoBehaviour
+{
+    public InteractionPolicy interactionPolicy;
+
+    protected bool enable = true;
+
+    public virtual void Enable()
+    {
+        enable = true;
+    }
+
+    public virtual void Disable()
+    {
+        enable = false;
+    }
+
+    protected void Update()
+    {
+        if (!enable) return;
+    }
+}
